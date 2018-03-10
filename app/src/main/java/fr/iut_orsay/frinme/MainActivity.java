7637c1eb1e6f95bbbb9419734b6ad3fef42fa77c
Brocart Guillaume <guillaume.brocart@u-psud.fr>
2018-03-10 17:05:35 +0100

    Gestion du bouton retour pour les fragments

--- app/src/main/java/fr/iut_orsay/frinme/MainActivity.java
+++ app/src/main/java/fr/iut_orsay/frinme/MainActivity.java
@@ -66,13 +66,23 @@ public class MainActivity extends AppCompatActivity implements
         fabEvent = (FloatingActionButton) findViewById(R.id.fabEvent);
 
         fab.setOnClickListener(v -> {
-            if(!isFabOpen) showFabMenu(); else closeFabMenu(); });
-        fabContact.setOnClickListener(v ->
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new Contact()).commit()
-        );
-        fabEvent.setOnClickListener(v ->
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new Event()).commit()
-        );
+            if(!isFabOpen) showFabMenu();
+            else closeFabMenu();
+        });
+        fabContact.setOnClickListener(v -> {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, new Contact())
+                    .addToBackStack(null)
+                    .commit();
+            closeFabMenu();
+        });
+        fabEvent.setOnClickListener(v -> {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, new Event())
+                    .addToBackStack(null)
+                    .commit();
+            closeFabMenu();
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -113,23 +123,22 @@ public class MainActivity extends AppCompatActivity implements
         int id = item.getItemId();
 
         if (id == R.id.nav_contacts) {
-            Contact contactFrag = new Contact();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, contactFrag);
-            fragmentTransaction.commit();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, new Contact())
+                    .addToBackStack(null)
+                    .commit();
         } else if (id == R.id.nav_evts) {
-            EventList evtFrag = new EventList();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, evtFrag);
-            fragmentTransaction.commit();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, new EventList())
+                    .addToBackStack(null)
+                    .commit();
         } else if (id == R.id.nav_settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
+            startActivity(new Intent(this, SettingsActivity.class));
         } else if (id == R.id.nav_map) {
-            Map MapFrag = new Map();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, MapFrag);
-            fragmentTransaction.commit();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, new Map())
+                    .addToBackStack(null)
+                    .commit();
         }
 
         drawer.closeDrawer(GravityCompat.START);
@@ -147,10 +156,10 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     public void setEventFrag(){
-        Event evtFrag = new Event();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, evtFrag);
-        fragmentTransaction.commit();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new Event())
+                .addToBackStack(null)
+                .commit();
     }
 
     private void showFabMenu(){
