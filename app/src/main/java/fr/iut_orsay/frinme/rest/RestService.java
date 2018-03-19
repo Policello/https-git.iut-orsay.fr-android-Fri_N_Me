5735bd084e23c48a82ae3a34852bbfa1c99bfdce
Brocart Guillaume <guillaume.brocart@u-psud.fr>
2018-03-19 17:35:25 +0100

    Models pour REST dans /pojo

--- app/src/main/java/fr/iut_orsay/frinme/rest/RestService.java
+++ app/src/main/java/fr/iut_orsay/frinme/rest/RestService.java
@@ -1,5 +1,7 @@
 package fr.iut_orsay.frinme.rest;
 
+import fr.iut_orsay.frinme.rest.pojo.EventDetails;
+import fr.iut_orsay.frinme.rest.pojo.EventListDetails;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
