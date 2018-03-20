bdca37efa1bed83b345a486d473789936bacf5d0
Giordano Martin <martin.giordano@u-psud.fr>
2018-03-20 11:12:13 +0100

    SPLASH

---
+++ app/src/main/java/fr/iut_orsay/frinme/SplashActivity.java
@@ -0,0 +1,30 @@
+package fr.iut_orsay.frinme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import fr.iut_orsay.frinme.MainActivity;
+
+/**
+ * Created by Martin on 20/03/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
