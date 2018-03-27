07d5d4603c51d54a707c1557970040f30fe22952
lcherch <liece.cherchour@u-psud.fr>
2018-03-27 09:51:06 +0200

    Appbar dans contacts
    thx Lièce

--- app/src/main/java/fr/iut_orsay/frinme/rest/RestService.java
+++ app/src/main/java/fr/iut_orsay/frinme/rest/RestService.java
@@ -1,6 +1,9 @@
 package fr.iut_orsay.frinme.rest;
 
+import fr.iut_orsay.frinme.rest.pojo.AddContact;
 import fr.iut_orsay.frinme.rest.pojo.Connexion;
+import fr.iut_orsay.frinme.rest.pojo.DeleteContact;
+import fr.iut_orsay.frinme.rest.pojo.EstAmi;
 import fr.iut_orsay.frinme.rest.pojo.EventDetails;
 import fr.iut_orsay.frinme.rest.pojo.EventListDetails;
 import fr.iut_orsay.frinme.rest.pojo.ContactListDetails;
@@ -23,8 +26,26 @@ public interface RestService {
     Call<ContactListDetails> getContactDetailedList(@Field("idUser") int idUser);
 
     @FormUrlEncoded
+    @POST("contact/deleteContact.php")
+    Call<DeleteContact> getDeleteFriend(@Field("idUser") int idUser, @Field("idUserDeleted") int idUserDeleted);
+
+    @FormUrlEncoded
+    @POST("contact/addContact.php")
+    Call<AddContact> getAddFriend(@Field("idUser") int idUser, @Field("idUserAdded") int idUserAdded);
+
+    @FormUrlEncoded
+    @POST("user/estAmi.php")
+    Call<EstAmi> getEstAmi(@Field("idUser") int idUser, @Field("idFriend") int idFriend);
+
+    @FormUrlEncoded
     @POST("user/login/")
     Call<Connexion> checkLogin(@Field("mail") String mail, @Field("mdpasse") String mdpasse);
+
+    @FormUrlEncoded
+    @POST("user/inscription.php")
+    Call<Connexion> addUser(@Field("mail") String mail, @Field("mdpasse") String mdpasse,
+                            @Field("pseudo") String pseudo, @Field("commentaire") String commentaire,
+                            @Field("latitude") Long latitude, @Field("longitude") Long longitude);
 
 
 }
---
+++ app/src/main/java/fr/iut_orsay/frinme/rest/pojo/EstAmi.java
@@ -0,0 +1,19 @@
+package fr.iut_orsay.frinme.rest.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by yyang5 on 23/03/2018.
+ */
+
+public class EstAmi {
+
+    @SerializedName("message")
+    @Expose
+    private boolean message;
+
+    public boolean getMessage() {
+        return message;
+    }
+}
