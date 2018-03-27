2f49feb7365bc967a5759b04d2ceca328cccd22d
Brocart Guillaume <guillaume.brocart@u-psud.fr>
2018-03-27 20:42:16 +0200

    fix build

--- app/src/main/java/fr/iut_orsay/frinme/view/CustomInfoWindowGoogleMap.java
+++ app/src/main/java/fr/iut_orsay/frinme/view/CustomInfoWindowGoogleMap.java
@@ -14,17 +14,16 @@ import fr.iut_orsay.frinme.R;
  * Created by cyan on 27/03/2018.
  */
 
-public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter{
+public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter {
     private Context context;
     private Object o;
     TextView dialog_msg, dialog_title, dialog_ok;
 
-    public CustomInfoWindowGoogleMap(Context ctx, Object o){
-             context =ctx;
-            this.o = o;
-
-
+    public CustomInfoWindowGoogleMap(Context ctx, Object o) {
+        context = ctx;
+        this.o = o;
     }
+
     @Override
     public View getInfoWindow(Marker marker) {
         return null;
@@ -34,16 +33,14 @@ public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter{
     public View getInfoContents(Marker marker) {
         /*switch (getObject().getClass().getSimpleName()) {
             case
-
         }*/
-       View view=((Activity)context).getLayoutInflater().inflate(R.layout.item_dialog, null);
-        dialog_msg = (TextView)view.findViewById(R.id.dialog_msg);
-        dialog_title = (TextView)view.findViewById(R.id.dialog_title);
+        View view = ((Activity) context).getLayoutInflater().inflate(R.layout.item_dialog, null);
+        dialog_msg = (TextView) view.findViewById(R.id.dialog_msg);
+        dialog_title = (TextView) view.findViewById(R.id.dialog_title);
         dialog_ok = (TextView) view.findViewById(R.id.dialog_ok);
         dialog_title.setText("information");
-        dialog_msg.setText();
+        dialog_msg.setText("");
         dialog_ok.setText("voir les details");
-
 
         return view;
     }
@@ -51,9 +48,5 @@ public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter{
     public Object getObject() {
         return (this.o);
     }
-
-
-
-
 
 }
